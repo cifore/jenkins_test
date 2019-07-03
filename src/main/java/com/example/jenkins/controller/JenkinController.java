801d3ab2cdd753db6d913f1c6f6187752eaab047
tianzhao <tianzhao@cc.com>
2019-07-03 10:23:39 +0800

    tz

--- src/main/java/com/example/jenkins/controller/JenkinController.java
+++ src/main/java/com/example/jenkins/controller/JenkinController.java
@@ -1,6 +1,5 @@
 package com.example.jenkins.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,9 +11,7 @@ public class JenkinController {
 
     @RequestMapping(value = "/jenkins",method = RequestMethod.GET)
     @ResponseBody
-    public JSONObject test(){
-        JSONObject object = new JSONObject();
-        object.put("name","hello,jenkins init success!!");
-        return object;
+    public String test(){
+        return "hello,jenkins init success！！！";
     }
 }
